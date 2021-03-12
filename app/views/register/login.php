f3ac3782334ab6b1fe2948f30514cae0876b1c9a
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-03-12 13:00:46 +0200

    CSS for login

--- app/views/register/login.php
+++ app/views/register/login.php
@@ -4,24 +4,24 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 <!-- <h1 class="text-center red">Welcome to the login page!</h1> -->
- <div>
+ <div class="login-div">
      <form action="<?PROOT?>register/login" method="post">
-         <h3>Log in</h3>
-         <div>
+         <h2 class="text-center">Log in</h2>
+         <div class="text-center margins-form">
              <label for="username">Username</label>
              <input type="text" name="username" id="username">
          </div>
-         <div>
+         <div class="text-center margins-form">
              <label for="password">Password</label>
              <input type="password" name="password" id="password">
          </div>
-         <div>
+         <div class="remember-me" >
              <label for="remember_me">Remember Me <input type="checkbox" id="remember_me" name="remember_me" value=""on></label>
          </div>
          <div>
              <input type="submit" value="Login">
          </div>
-         <div>
+         <div class="text-center line-block-reg">
              <a href="<?=PROOT?>register/register">Register</a>
          </div>
      </form>
