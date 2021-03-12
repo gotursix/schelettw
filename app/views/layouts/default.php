d93f7fec145eefef8d3eec7ef9baa57056f3cfcb
Serban Mihai <mihai.serban@info.uaic.ro>
2021-03-12 13:27:44 +0200

    add nav design

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -10,6 +10,22 @@
     <?= $this->content('head');?>
 </head>
 <body>
+    <nav class="nav">
+        <ul class="nav__list nav__list-left">
+
+                <li class="nav__list--button"><a href="#">Home</a></li>
+                <li class="nav__list--button"><a href="#">Rankings</a></li>
+        </ul>
+        <ul class="nav__list nav__list-right">
+            <img
+                src="img/default-user-image.png"
+                alt="user image"
+                class="user-nav__user-photo"
+            />
+            <span class="user-nav__user-name">User name</span>
+        </ul>
+    </nav>
+
     <?= $this->content('body');?>
 </body>
 </html>
