524e3af512a1803853c1d77e32d918a90d8881e5
Valentin <vly.2814@gmail.com>
2021-03-12 14:01:20 +0200

    Custom js

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,31 +1,32 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title><?=$this->getSiteTitle();?></title>
+    <title><?= $this->getSiteTitle(); ?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSS -->
-    <link rel="stylesheet" href="<?=PROOT?>css/custom.css">
-    <?= $this->content('head');?>
+    <link rel="stylesheet" href="<?= PROOT ?>css/custom.css">
+    <!-- JS -->
+    <script src="<?= PROOT ?>js/custom.js"></script>
+    <?= $this->content('head'); ?>
 </head>
 <body>
-    <nav class="nav">
-        <ul class="nav__list nav__list-left">
+<nav class="nav">
+    <ul class="nav__list nav__list-left">
+        <li class="nav__list--button"><a href="#">Home</a></li>
+        <li class="nav__list--button"><a href="#">Rankings</a></li>
+    </ul>
+    <ul class="nav__list nav__list-right">
+        <img
+            src="img/default-user-image.png"
+            alt="user image"
+            class="user-nav__user-photo"
+        />
+        <span class="user-nav__user-name">User name</span>
+    </ul>
+</nav>
 
-                <li class="nav__list--button"><a href="#">Home</a></li>
-                <li class="nav__list--button"><a href="#">Rankings</a></li>
-        </ul>
-        <ul class="nav__list nav__list-right">
-            <img
-                src="img/default-user-image.png"
-                alt="user image"
-                class="user-nav__user-photo"
-            />
-            <span class="user-nav__user-name">User name</span>
-        </ul>
-    </nav>
-
-    <?= $this->content('body');?>
+<?= $this->content('body'); ?>
 </body>
 </html>
