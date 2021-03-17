2f6c6ef9f861095bea9d80cf7309442dd29690bc
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-03-17 09:53:54 +0200

    login/register bottom margins

--- app/views/register/login.php
+++ app/views/register/login.php
@@ -6,7 +6,7 @@
 <!-- <h1 class="text-center red">Welcome to the login page!</h1> -->
 <div class="login-page">
     <div class="form">
-        <h2 class="text-center">Log in</h2>
+        <h2 class="text-center-r">Log in</h2>
         <form class="login-form" action="<?=PROOT?>register/login" method="post">
             <input type="text" placeholder="Username"/>
             <input type="password" placeholder="Password"/>
--- app/views/register/register.php
+++ app/views/register/register.php
@@ -5,15 +5,15 @@
 <?php $this->start('body'); ?>
 <div class="login-page">
     <div class="form">
-        <h2 class="text-center">Register now!</h2>
-        <form class="register-form">
-            <input type="text" placeholder="First name"/>
-            <input type="text" placeholder="Last name"/>
-            <input type="text" placeholder="Email address"/>
-            <input type="text" placeholder="Choose a Username"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Confirm password"/>
-            <button>create</button>
+        <h2 class="text-center-r">Register now!</h2>
+        <form class="register-form" action="" method="post">
+            <input type="text" id="fname" placeholder="First name"/>
+            <input type="text" id="lname" placeholder="Last name"/>
+            <input type="text" id="email" placeholder="Email address"/>
+            <input type="text" id="username" placeholder="Choose a Username"/>
+            <input type="password" id="password" placeholder="Password"/>
+            <input type="password" id="confirm" placeholder="Confirm password"/>
+            <button type="submit">Register</button>
             <p class="message">Already registered? <a href="<?=PROOT?>register/login">Sign In</a></p>
         </form>
     </div>
