118a94011988e44e0922061b17c754ad9f0f4721
Valentin <vly.2814@gmail.com>
2021-03-25 16:31:47 +0200

    Auto stash before merge of "master" and "origin/master"

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,4 @@
 <?php $this->setSiteTitle('Home'); ?>
-<?php $this->start('head'); ?>
-
-<?php $this->end(); ?>
 <?php $this->start('body'); ?>
 <h1 class="text-center red">Welcome to the best website from the world</h1>
 
