2639efbd94225f261e7520911bd40ca6ce5ec82c
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-03-25 20:36:09 +0200

    3Buttons red green purple

--- app/views/home/instructions.php
+++ app/views/home/instructions.php
@@ -2,7 +2,10 @@
 <?php $this->start('body'); ?>
 <h1 class="text-center red">Learn how to play</h1>
 
-// butoane
+<!--// butoane
 // clasa container margini stanga dreapta -- container css
-// footer, coloane responsive?
+// footer, coloane responsive? -->
+<button id="buttonPurple">First button</button>
+<button id="buttonGreen">Second button</button>
+<button id="buttonRed">Third button</button>
 <?php $this->end(); ?>
