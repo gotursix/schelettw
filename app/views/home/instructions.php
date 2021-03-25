1bad447b15eba3a4db657887618ef8b575fec6b7
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-03-25 20:11:49 +0200

    Css- radius buttons and container
    Just changed radius border for buttons and containers in login and register

--- app/views/home/instructions.php
+++ app/views/home/instructions.php
@@ -2,4 +2,7 @@
 <?php $this->start('body'); ?>
 <h1 class="text-center red">Learn how to play</h1>
 
+// butoane
+// clasa container margini stanga dreapta -- container css
+// footer, coloane responsive?
 <?php $this->end(); ?>
