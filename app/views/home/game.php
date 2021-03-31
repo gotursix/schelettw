d6a6fa12305a5c420d7c79d96107282b38a14e1b
Serban Mihai <ser13ban@gmail.com>
2021-03-31 11:21:09 +0300

    added the schelet for the game page

--- app/views/home/game.php
+++ app/views/home/game.php
@@ -1,6 +1,20 @@
 <?php $this->setSiteTitle('Home'); ?>
+<?php $this->start('head'); ?>
+<script src="<?= PROOT ?>js/instructionScript.js" defer></script>
+<?php $this->end(); ?>
 <?php $this->start('body'); ?>
 <div class="container content">
     <h1 class="text-center red">Welcome to the best fruits game!</h1>
+
+    <div class="container-buttons">
+        <a id="easy" href="#" class="difficulty-button-instruction difficulty-button button-easy" >Easy</a>
+        <a id="medium" href="#" class="difficulty-button-instruction difficulty-button button-medium" >Medium</a>
+        <a id="hard" href="#" class="difficulty-button-instruction difficulty-button button-hard" >Hard</a>
+    </div>
+    
+    <button id="wrong-answer1" class="buttonPurple" >pumpkin</button>
+    <button id="wrong-answer2" class="buttonPurple" >Kiwi</button><br>
+    <button id="correct-answer" class="buttonPurple">Watermelone</button>
+    <button id="wrong-answer3" class="buttonPurple">Cucumbern</button>
 </div>
 <?php $this->end(); ?>
