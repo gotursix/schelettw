5658f414c7d7792195089a9ffdb858a015748e13
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-04-02 23:54:04 +0300

    css for game page

--- app/views/home/game.php
+++ app/views/home/game.php
@@ -8,19 +8,21 @@ use Core\H;
 <script src="<?= PROOT ?>js/instructionScript.js" defer></script>
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
-<div class="container content text-center">
+<div class="container content  center text-center margin-btm">
     <h1 class="text-center red">Welcome to the best fruits game!</h1>
+    <br>
 
     <div class="container-buttons" id="diff-buttons">
         <a id="easy" href="#" class="difficulty-button-instruction difficulty-button button-easy" >Easy</a>
         <a id="medium" href="#" class="difficulty-button-instruction difficulty-button button-medium" >Medium</a>
         <a id="hard" href="#" class="difficulty-button-instruction difficulty-button button-hard" >Hard</a>
     </div>
+    <br>
     <?php
         $url = FH::generateImage('watermelon');
     ?>
 
-    <img src="<?=FH::generateImage('watermelon') ?>" alt="watermelone" class="game-image"><br>
+    <img src="<?=FH::generateImage('watermelon') ?>" alt="watermelone" class="game-image"><br><br>
     <button data-modal-target="#modal" id="wrong-answer1" class="buttonPurple" >Pumpkin</button>
     <button data-modal-target="#modal" id="wrong-answer2" class="buttonPurple" >Kiwi</button><br>
     <button data-modal-target="#modal" id="correct-answer" class="buttonPurple">Watermelone</button>
