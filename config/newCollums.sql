95aa5641da7e60a46a3d919e214b47dc91126262
Valentin <vly.2814@gmail.com>
2021-05-24 15:28:47 +0300

    Added table

--- config/newCollums.sql
+++ config/newCollums.sql
@@ -3,3 +3,17 @@ ALTER TABLE `users` ADD `banned` BOOLEAN NOT NULL DEFAULT FALSE AFTER `acl`;
 
 --and this one for the profile photo of the alter user
 ALTER TABLE `users` ADD `photoId` INT NOT NULL DEFAULT '0' AFTER `banned`;
+
+--and this for adding questions table
+CREATE TABLE `questions` (
+                             `id` int(100) NOT NULL,
+                             `photo` varchar(100) NOT NULL,
+                             `continent` varchar(100) NOT NULL,
+                             `question` text NOT NULL,
+                             `answer1` varchar(200) NOT NULL,
+                             `answer2` varchar(200) NOT NULL,
+                             `answer3` varchar(200) NOT NULL,
+                             `answer4` varchar(200) NOT NULL,
+                             `correct` int(100) NOT NULL,
+                             `header` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
